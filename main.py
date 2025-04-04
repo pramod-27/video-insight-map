84bfa3dfc8b28ad2b8d0c8596a6bfe8396075639
pramod <pramodkumarmarri711@gmail.com>
2025-04-05 03:01:13 +0530

    Set keep-alive to 10min

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ async def health():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))
     logger.info(f"Starting on port {port}")
-    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info", workers=1, timeout_keep_alive=300)  # Max keep-alive
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info", workers=1, timeout_keep_alive=600)  # 10min keep-alive
